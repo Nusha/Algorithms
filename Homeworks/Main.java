6d158e72c08e6e054b1333b3f8a862e759689b0c
Ivan Balabay <i.balabai@gmail.com>
2023-04-13 03:54:08 +0300

    Task 03 for Algorithms completed;

---
+++ Homeworks/Main.java
@@ -0,0 +1,71 @@
+import java.util.Random;
+
+public class Main {
+
+    static Node head;
+    Node current;
+
+    static class Node extends Main {
+        int value;
+        Node next;
+
+
+        Node(int v) {
+            value = v;
+            next = null;
+        }
+    }
+
+    private static Node reverse(Node node) {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+    }
+
+    public static void printList(Node node) {
+        while (node != null) {
+            if (node == head) {
+                System.out.printf(" " + node.value);
+            } else {
+                System.out.printf(" -> " + node.value);
+            }
+            node = node.next;
+        }
+    }
+
+    public static Main fillNodes(int qtt) {
+
+        Main list = new Main();
+        Random rnd = new Random();
+
+        head = new Node(rnd.nextInt(100));
+        list.current = head;
+        for (int i = 0; i < qtt - 1; i++) {
+            int v = rnd.nextInt(100);
+            list.current.next = new Node(v);
+            list.current = list.current.next;
+
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        fillNodes(12);
+        System.out.println("Randomly generated list below:");
+        printList(head);
+        System.out.println("");
+        head = reverse(head);
+        System.out.println("Reversed list below:");
+        printList(head);
+    }
+}
+
+
